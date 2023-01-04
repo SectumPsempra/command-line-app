2238b564f3c3d5be3f4bb8995967fae2479a6e7c
Amit Bhatt <bhattamit991@gmail.com>
2023-01-04 18:23:36 +0530

    fix file name, build file

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pip3 install -r requirements.txt
-python3 -m app sample_input/input1.txt
+python3 -m geektrust sample_input/input1.txt
 python3 -m unittest
 python3 -m coverage run -m unittest
 python3 -m coverage report -m
--- src/course_manager.py
+++ src/course_manager.py
@@ -15,6 +15,19 @@ class CourseManager:
 
     CANCEL_ACCEPTED = "CANCEL_ACCEPTED"
     CANCEL_REJECTED = "CANCEL_REJECTED"
+    
+    title = "title"
+    instructor = "instructor"
+    min_emp = "min_emp"
+    max_emp = "max_emp"
+    slots_left = "slots_left"
+    date = "date"
+    final_status = "final_status"
+    alloted = "alloted"
+    course_registered_ids = "course_registered_ids"
+    
+    course_id = "course_id"
+    email_id = "email_id"
 
     def __init__(self):
         """constructor of CourseManager class. Initializes class and keeps
@@ -45,15 +58,15 @@ class CourseManager:
             title, instructor
         )
         self.courses[course_id] = {
-            "title": title,
-            "instructor": instructor,
-            "min_emp": min_emp,
-            "max_emp": max_emp,
-            "slots_left": max_emp,
-            "date": date,
-            "final_status": None,
-            "alloted": False,
-            "course_registered_ids": set()
+            CourseManager.title: title,
+            CourseManager.instructor: instructor,
+            CourseManager.min_emp: min_emp,
+            CourseManager.max_emp: max_emp,
+            CourseManager.slots_left: max_emp,
+            CourseManager.date: date,
+            CourseManager.final_status: None,
+            CourseManager.alloted: False,
+            CourseManager.course_registered_ids: set()
         }
         print(course_id)
 
@@ -72,16 +85,18 @@ class CourseManager:
         """
         course = self.courses.get(course_id)
         emp_name = email_id.split("@")[0]
-        course_name = course.get("title")
+        course_name = course.get(CourseManager.title)
         course_registration_id = CourseManager.ID_COURSE_REGISTRATION.format(
             emp_name, course_name)
-        if course.get("slots_left") > 0:
-            course["slots_left"] = course["slots_left"] - 1
+        if course.get(CourseManager.slots_left) > 0:
+            course[CourseManager.slots_left] = \
+                course[CourseManager.slots_left] - 1
             self.course_reg[course_registration_id] = {
-                "course_id": course_id,
-                "email_id": email_id
+                CourseManager.course_id: course_id,
+                CourseManager.email_id: email_id
             }
-            course["course_registered_ids"].add(course_registration_id)
+            course[CourseManager.course_registered_ids].add(
+                course_registration_id)
             print(f"{course_registration_id} {CourseManager.STATUS_ACCEPTED}")
         else:
             print(CourseManager.STATUS_COURSE_FULL)
@@ -100,21 +115,27 @@ class CourseManager:
             dict: updated courses.
         """
         course = self.courses.get(course_id)
-        if course.get("max_emp") - course.get("slots_left") < \
-                course.get("min_emp"):
-            course["final_status"] = CourseManager.FINAL_STATUS_CANCELED
+        if course.get(CourseManager.max_emp) - \
+                course.get(CourseManager.slots_left) < \
+                    course.get(CourseManager.min_emp):
+            course[CourseManager.final_status] = \
+                CourseManager.FINAL_STATUS_CANCELED
         else:
-            course["final_status"] = CourseManager.FINAL_STATUS_CONFIRMED
-            course["alloted"] = True
-        registered_course_ids = sorted(course.get("course_registered_ids"))
+            course[CourseManager.final_status] = \
+                CourseManager.FINAL_STATUS_CONFIRMED
+            course[CourseManager.alloted] = True
+        registered_course_ids = sorted(course.get(
+            CourseManager.course_registered_ids))
         for reg_id in registered_course_ids:
-            email = self.course_reg.get(reg_id).get("email_id")
-            print(f'{reg_id} {email} {course_id} {course.get("title")}'
-                  f' {course.get("instructor")} {course.get("date")}'
-                  f' {course.get("final_status")}')
+            email = self.course_reg.get(reg_id).get(CourseManager.email_id)
+            print(f'{reg_id} {email} {course_id}'
+                  f' {course.get(CourseManager.title)}'
+                  f' {course.get(CourseManager.instructor)}'
+                  f' {course.get(CourseManager.date)}'
+                  f' {course.get(CourseManager.final_status)}')
         self.courses[course_id] = course
 
-    
+
     def cancel(self, course_reg_id: str) -> None:
         """Cancel a registered course if it is not alloted
 
@@ -126,13 +147,14 @@ class CourseManager:
         Returns:
             tuple: updated courses and employee registrations.
         """
-        course_id = self.course_reg.get(course_reg_id).get("course_id")
+        course_id = self.course_reg.get(course_reg_id).get(CourseManager.course_id)
         course = self.courses.get(course_id)
-        if course.get("alloted"):
+        if course.get(CourseManager.alloted):
             print(f"{course_reg_id} {CourseManager.CANCEL_REJECTED}")
         else:
-            course.get("course_registered_ids").remove(course_reg_id)
-            course["slots_left"] += 1
+            course.get(CourseManager.course_registered_ids).remove(
+                course_reg_id)
+            course[CourseManager.slots_left] += 1
             self.course_reg.pop(course_reg_id)
             print(f"{course_reg_id} {CourseManager.CANCEL_ACCEPTED}")
         self.courses[course_id] = course
