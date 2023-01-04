2f337ef4d707fe961307be609c850fe88ca4a78a
Amit Bhatt <bhattamit991@gmail.com>
2023-01-05 03:09:50 +0530

    fix if for

--- utils/utils.py
+++ utils/utils.py
@@ -4,85 +4,93 @@ import os
 import datetime
 
 def validate_args(args: list) -> None:
-    if len(args) != len(config.SUPPORTED_APP_ARGS):
-        err_message = (
-            "File path not entered or extra arguments passed."
-            f" Expected arguments are: {config.SUPPORTED_APP_ARGS}."
-        )
+    err_message = (
+        "File path not entered or extra arguments passed."
+        f" Expected arguments are: {config.SUPPORTED_APP_ARGS}."
+    )
+    try:
+        assert len(args) == len(config.SUPPORTED_APP_ARGS)
+    except AssertionError:
         raise exceptions.INPUT_DATA_ERROR(message=err_message)
+    except Exception as err:
+        raise exceptions.INPUT_DATA_ERROR(message=err)
 
 def validate_path(path: str) -> None:
-    base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    file_path = os.path.join(base_dir, path)
-    if not os.path.isfile(file_path):
-        err_message = "File path is not valid or file doesn't exist"
+    err_message = "File path is not valid or file doesn't exist"
+    try:
+        base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        file_path = os.path.join(base_dir, path)
+        assert os.path.isfile(file_path)
+        return file_path
+    except AssertionError:
         raise exceptions.INPUT_DATA_ERROR(message=err_message)
-    return file_path
+    except Exception as err:
+        raise exceptions.INPUT_DATA_ERROR(message=err)
 
 def detect_command(line: str, separator: str=" ") -> list:
     args = line.split(sep=separator)
     return args
 
-def _validate_add(args: list) -> None:
-    _date, _min, _max = args[-3], args[-2], args[-1]
-    if _min < 1:
-        raise exceptions.INPUT_DATA_ERROR(
-            message="Minimum one enrollment must be done"
-        )
-    if _min > _max:
-        raise exceptions.INPUT_DATA_ERROR(
-            message="Max employees can not be less than min employees"
-        )
-    if len(_date) != 8:
-        raise exceptions.INPUT_DATA_ERROR(
-            message="Date should be specified in ddmmyyyy format"
-        )
+def _validate_add(args: list, command: str) -> None:
+    if command != config.ADD:
+        return
+    err_message = ""
     try:
-        datetime.datetime(day=int(_date[:2]),
-                            month=int(_date[2:4]),
-                            year=int(_date[4:]))
+        _date, _min, _max = args[-3], args[-2], args[-1]
+        err_message = "Minimum one enrollment must be done"
+        assert _min >= 1
+        err_message = "Max employees can not be less than min employees"
+        assert _min <= _max
+        err_message = "Date should be specified in ddmmyyyy format"
+        assert len(_date) == 8
+        _ = datetime.datetime(day=int(_date[:2]),
+                                month=int(_date[2:4]),
+                                year=int(_date[4:]))
+    except AssertionError:
+        raise exceptions.INPUT_DATA_ERROR(message=err_message)
     except Exception as err:
-        raise exceptions.INPUT_DATA_ERROR(
-            message=err
-        )
+        raise exceptions.INPUT_DATA_ERROR(message=err)
 
 def validate_command(args: list) -> list:
-    cmd_name = args[0]
-    updated_args = args[1:]
-    cmd_metadata = config.COMMANDS_METADATA.get(cmd_name)
-    if not cmd_metadata or len(cmd_metadata) != len(updated_args):
+    try:
+        cmd_name = args[0]
+        updated_args = args[1:]
+        cmd_metadata = config.COMMANDS_METADATA.get(cmd_name)
         err_message = (
             "Please specify a valid command and ensure its argument"
             " are in correct order. List of valid commands and the"
             f" parameters they support:\n{config.SUPPORTED_COMMANDS}"
         )
+        assert cmd_metadata
+        assert len(cmd_metadata) == len(updated_args)
+
+        updated_args = [
+            typ(val.strip()) if not isinstance(val, typ) else val.strip() \
+                for typ, val in zip(cmd_metadata, updated_args)
+        ]
+
+        _validate_add(args=updated_args, command=cmd_name)
+        return cmd_name, updated_args
+    except AssertionError:
         raise exceptions.INPUT_DATA_ERROR(message=err_message)
-    for index in range(len(updated_args)):
-        param_type = cmd_metadata[index]
-        param_value = updated_args[index].strip()
-        if not isinstance(param_value, param_type):
-            updated_args[index] = param_type(param_value)
-        else:
-            updated_args[index] = param_value
-    if cmd_name == config.ADD:
-        _validate_add(args=updated_args)
-    return cmd_name, updated_args
+    except Exception as err:
+        raise err
 
 def parse_file(arguments):
     validate_args(args=arguments)
-    file_path = arguments[1]
-    abs_file_path = validate_path(file_path)
-    file_handle = open(abs_file_path, 'r')
-    lines = file_handle.readlines()
-    if not lines:
-        err_message = (
-            "Input file should have valid content."
-        )
+    err_message = (
+        "Input file should have valid content."
+    )
+    try:
+        file_path = arguments[1]
+        abs_file_path = validate_path(file_path)
+        file_handle = open(abs_file_path, 'r')
+        lines = file_handle.readlines()
+        assert lines
+        return lines
+    except AssertionError:
         raise exceptions.INPUT_DATA_ERROR(message=err_message)
-    return lines
 
 def print_output(result) -> None:
-    if isinstance(result, str):
-        print(result)
-    elif isinstance(result, tuple) or isinstance(result, list):
-        print("\n".join(result))
+    res_type = {str: result, tuple: "\n".join(result), list: "\n".join(result)}
+    print(res_type.get(type(result)))
